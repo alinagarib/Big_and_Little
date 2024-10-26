8d51e553bbfd46de2867cdf5e17d9f6e26f8e886
Steve Sajeev <stevesajeev123@gmail.com>
2024-10-26 01:17:02 -0400

    Create app structure and login page

--- mobile/app/(auth)/register.js
+++ mobile/app/(auth)/register.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+/*
+  Route: /register
+
+  Prompts user to create their account
+
+  CURRENTLY A PLACEHOLDER
+*/
+export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Find your fam! Maybe?</Text>
-      <StatusBar style="auto" />
+      <Text>Placeholder</Text>
     </View>
   );
 }
