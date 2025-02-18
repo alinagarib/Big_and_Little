bd64ba9b7719ab09251bfee52606f40e7d21ac07
ahhhh W <noahwhelden@gmail.com>
2025-02-18 00:17:49 -0500

    Add org id collection

--- server/controllers/orgController.js
+++ server/controllers/orgController.js
@@ -8,6 +8,7 @@ const getOrganizations = async (req, res) => {
     try {
         const orgs = await Organization.find({ isPublic: true, isMatching: false }).exec();
         const orgsArray = orgs.map(org => ({
+            id: org.id,
             name: org.name,
             description: org.description,
             logo: org.logo,
