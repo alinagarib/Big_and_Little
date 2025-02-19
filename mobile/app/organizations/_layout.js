e4e1692ab25c138cc31f67408d53e31184258c06
Steve Sajeev <stevesajeev123@gmail.com>
2025-02-19 14:18:03 -0500

    Protect organization pages from the frontend and check JWT expiry

--- mobile/app/organizations/_layout.js
+++ mobile/app/organizations/_layout.js
@@ -1,16 +1,26 @@
 import { Text } from 'react-native';
-import { Redirect, Stack } from 'expo-router';
+import { Redirect, useLocalSearchParams, Stack } from 'expo-router';
 
+import useAuth from '@context/useAuth';
 import { useSession } from '@context/ctx';
 
 export default function OrganizationsLayout() {
   const { session, isLoading } = useSession();
-
+  
   if (isLoading) {
     return <Text>Loading...</Text>;
   }
+
+  // User is not logged in
   if (!session) {
     return <Redirect href="/" />;
+  }
+
+  // User is logged in, but not in the organization
+  const { _, profiles } = useAuth();
+  const { org } = useLocalSearchParams();
+  if (!profiles.some(profile => profile.organizationId === org)) {
+    return <Redirect href="/home" />;
   }
 
   return (
